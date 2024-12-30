be75effc50b5e2bcf3e4962ac21a49872896a119
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-30 23:04:39 +0600

    Basic Project Setup, Start -

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <>
+    <h1 className='text-3xl text-center my-6 bg-blue-500 text-white p-3 rounded-2xl'>Sheikh muhammad Antor</h1>
+    </>
+  )
+}
+
+export default App
