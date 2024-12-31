fc08e69da3a83a5208937f0f32e2ca07923ffcd7
Sheikh Muhammad Antor <sheikhmuhammadantor@gmail.com>
2024-12-31 14:46:43 +0600

    Setup basic route , home component.

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -4,6 +4,9 @@ function Home() {
     return <div>
         <h1>Home</h1>
         <Link to="/app" className="btn">APP</Link>
+        <div>
+            <img src="https://github.com/ProgrammingHero1/bistro-boss-restaurant-resources/blob/main/assets/home/01.jpg" alt="" />
+        </div>
     </div>
 }
 
